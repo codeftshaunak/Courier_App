3af1dfdf40cef28ac0f158e145a64b59887fcf4f
Shaunak KD <sagardeyyash19@gmail.com>
2023-05-02 17:01:37 +0600

    about us page

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from 'react';
 import LayoutComponent from '../layout/LayoutComponent';
 import Hero from './Hero';
 import Footer from './Footer';
+import HeroAbou from './HeroAbou';
 
 const About = () => {
     return (
         <>
             <LayoutComponent>
+                <HeroAbou />
                 <Footer />
             </LayoutComponent>
         </>
---
+++ src/components/HeroAbou.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroAbou = () => {
+
+
+    return (
+        <div className="about vh-80 mt-20 relative">
+            <div className="about-image"></div>
+            <div className="about-content">
+                <div className="text-about-content">
+                    <h1>ABOUT APL LOGISTICS</h1>
+                    <p>A leading provider of global supply chain logistics.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroAbou;
