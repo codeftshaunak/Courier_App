550a73752288ccfe21edbeb89ad02cda0ace2e83
Shaunak KD <sagardeyyash19@gmail.com>
2023-05-02 17:33:08 +0600

    add contact.js & contactform.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import LayoutComponent from '../layout/LayoutComponent';
+import ContactForm from './ContactForm';
 
 const Contact = () => {
-  return (
-    <LayoutComponent>
-      
-    </LayoutComponent>
-  )
+    return (
+        <LayoutComponent>
+            <div className="about vh-80 mt-20 relative">
+                <div className="about-image"></div>
+                <div className="about-content">
+                    <div className="text-about-content">
+                        <h1>Contact Us</h1>
+                        <ContactForm />
+                    </div>
+                </div>
+            </div>
+        </LayoutComponent>
+    )
 }
 
 export default Contact;
