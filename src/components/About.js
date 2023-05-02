b5c0b9d52fe515665336c22fab48d0be969669b5
Shaunak KD <sagardeyyash19@gmail.com>
2023-05-02 15:10:55 +0600

    footer add

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LayoutComponent from '../layout/LayoutComponent';
 import Hero from './Hero';
+import Footer from './Footer';
 
 const About = () => {
     return (
         <>
             <LayoutComponent>
+                <Footer />
             </LayoutComponent>
         </>
 
