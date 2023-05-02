b3b458a9c21c369189cb8f1efd32899a2ed443db
Shaunak KD <sagardeyyash19@gmail.com>
2023-05-02 20:29:49 +0600

    project tracking

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,8 @@ import ContactForm from './ContactForm';
 const Contact = () => {
     return (
         <LayoutComponent>
-            <div className="about vh-80 mt-20 relative">
-                <div className="about-image"></div>
-                <div className="about-content">
-                    <div className="text-about-content">
-                        <h1>Contact Us</h1>
-                        <ContactForm />
-                    </div>
-                </div>
+            <div className="vh-80 mt-20 bg-color pt-10">
+                <ContactForm />
             </div>
         </LayoutComponent>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
     { name: 'Home', href: '/' },
     { name: 'About Us', href: 'about' },
     { name: 'Contact Us', href: 'contact' },
-    { name: 'Tract & Trace', href: '#' },
 ]
 
 export default function Navbar() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const [open, setOpen] = useState(false)
+    const cancelButtonRef = useRef(null)
 
     return (
         <div className="bg-white overflow-hidden">
@@ -43,12 +45,18 @@ export default function Navbar() {
                             </a>
                         ))}
                     </div>
+
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                         <a href="signin" className="text-sm font-semibold leading-6 text-gray-900">
                             Sign In <span aria-hidden="true">&rarr;</span>
                         </a>
+                        <button onClick={() => setOpen(true)} className="text-sm font-semibold leading-6 text-gray-900">
+                            Track Now <span aria-hidden="true">&rarr;</span>
+                        </button>
                     </div>
                 </nav>
+
+
                 <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                     <div className="fixed inset-0 z-50" />
                     <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -96,9 +104,99 @@ export default function Navbar() {
                         </div>
                     </Dialog.Panel>
                 </Dialog>
+
+
+
+                <Transition.Root show={open} as={Fragment}>
+                    <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        <div className="fixed inset-0 z-10 overflow-y-auto">
+                            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                >
+                                    <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                            <div className="sm:flex sm:items-start">
+                                                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                    <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                                </div>
+                                                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                                        Track Your Order Now                                                    </Dialog.Title>
+                                                    <div>
+                                                        <div className="mt-2.5">
+                                                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                                                                Select Tracking Type *
+                                                            </label>
+                                                            <input
+                                                                type="text"
+                                                                name="first-name"
+                                                                id="first-name"
+                                                                autoComplete="given-name"
+                                                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-100"
+                                                            />
+                                                        </div>
+                                                        <div className="mt-2.5">
+                                                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                                                                Enter your reference number for full visibility
+                                                            </label>
+                                                            <input
+                                                                type="text"
+                                                                name="first-name"
+                                                                id="first-name"
+                                                                autoComplete="given-name"
+                                                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-100"
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                            <button
+                                                type="button"
+                                                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                                onClick={() => setOpen(false)}
+                                            >
+                                                Track Now                                            </button>
+                                            <button
+                                                type="button"
+                                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                                onClick={() => setOpen(false)}
+                                                ref={cancelButtonRef}
+                                            >
+                                                Cancel
+                                            </button>
+                                        </div>
+                                    </Dialog.Panel>
+                                </Transition.Child>
+                            </div>
+                        </div>
+                    </Dialog>
+                </Transition.Root>
             </header>
 
 
         </div>
     )
 }
+
+
