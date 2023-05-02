ed592b76ce97ebe10a30b8efc02e054519a6d0e0
Shaunak KD <sagardeyyash19@gmail.com>
2023-05-02 17:18:18 +0600

    contact page

---
+++ pages/contact/index.js
@@ -0,0 +1,12 @@
+import Contact from '@/src/components/Contact';
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <Contact />
+        </div>
+    )
+}
+
+export default index;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayoutComponent from '../layout/LayoutComponent';
+
+const Contact = () => {
+  return (
+    <LayoutComponent>
+      
+    </LayoutComponent>
+  )
+}
+
+export default Contact;
