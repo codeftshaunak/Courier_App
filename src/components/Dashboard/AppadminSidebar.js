dba042d3f88e3d97a2f409a519f120cab5380aad
codeftshaunak <sagardeyyash19@gmail.com>
2023-08-05 18:23:59 +0600

    update the code

--- src/components/Dashboard/AppadminSidebar.js
+++ src/components/Dashboard/AppadminSidebar.js
@@ -47,7 +47,7 @@ const AppadminSidebar = () => {
                 <br />
                 <ul className='flex flex-col gap-y-2'>
                     {Menus.map((menu, index) => (
-                        <NextLink href={menu.path} key={index}>
+                        <NextLink to={menu.path} key={index}>
                             <li
                                 className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700
                              ${router.pathname === menu.path ? 'bg-gray-200 dark:bg-gray-700' : ''}`}
@@ -74,7 +74,7 @@ const AppadminSidebar = () => {
                         } absolute z-50 flex-col items-center self-end py-8 mt-16 space-y-6 font-bold sm:w-auto left-6 right-6 dark:text-white  bg-gray-50 dark:bg-slate-800 drop-shadow md rounded-xl`}
                 >
                     {Menus.map((menu, index) => (
-                        <NextLink href={menu.path} key={index}>
+                        <NextLink to={menu.path} key={index}>
                             <li
                                 className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700
                              ${router.pathname === menu.path ? 'bg-gray-200 dark:bg-gray-700' : ''}`}
